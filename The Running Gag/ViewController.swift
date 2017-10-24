23e7c69b5afad0b71d7225ac21bcf83014c2bff7
Daniel Espina <dannyj.espina@gmail.com>
2017-10-24 00:26:55 -0400

    Initial Commit

---
+++ The Running Gag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Running Gag
+//
+//  Created by Danny Espina on 10/24/17.
+//  Copyright © 2017 LegendarySilverback. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
